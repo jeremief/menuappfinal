152835ca150b7af52733f7d1cbd21387702d7598
jeremief <quasimobeau@yahoo.fr>
2016-05-22 14:51:00 +1000

    All templates are now plugged into the website

--- finalProject.py
+++ finalProject.py
@@ -18,33 +18,33 @@ def showRestaurants():
 
 @app.route('/restaurant/new/')
 def newRestaurant():
-    return "This page will be for making a new restaurant"
+    return render_template('newrestaurant.html')
 
 @app.route('/restaurant/<int:restaurant_id>/edit/')
 def editRestaurant(restaurant_id):
-    return "This page will be for editing restaurant %s" % restaurant_id
+    return render_template('editrestaurant.html')
 
 @app.route('/restaurant/<int:restaurant_id>/delete')
 def deleteRestaurant(restaurant_id):
-    return "This page will be for deleting restaurant %s" % restaurant_id
+    return render_template('deleterestaurant.html')
 
 @app.route('/restaurant/<int:restaurant_id>/')
 @app.route('/restaurant/<int:restaurant_id>/menu')
 def showMenu(restaurant_id):
-    return "This page is the menu for restaurant %s" % restaurant_id
+    return render_template('menu.html')
 
 @app.route('/restaurant/<int:restaurant_id>/menu/new')
 def newMenuItem(restaurant_id):
-    return "This page is for making a new menu item for restaurant %s" % restaurant_id
+    return render_template('newmenuitem.html')
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
 def editMenuItem(restaurant_id, menu_id):
-    return "This page is for editing menu %s for restaurant %s" % (menu_id, restaurant_id)
+    return render_template('editmenuitem.html')
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
 def deleteMenuItem(restaurant_id, menu_id):
-    return "This page is for deleting menu %s for restaurant %s" % (menu_id, restaurant_id)
+    return render_template('deletemenuitem.html')
 
 
 if __name__ == '__main__':
